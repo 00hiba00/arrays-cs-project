7d51ec89bf01fcd5d9f3de3b59b87e0089d01708
00hiba00 <hibaelbouanani123@gmail.com>
2025-10-04 18:07:36 +0100

    exercise 3

--- src/main/java/samplearrays/BankAccount.java
+++ src/main/java/samplearrays/BankAccount.java
@@ -4,26 +4,44 @@ public class BankAccount {
 
     String name;
     double currentBalance;
-    //TO-DO: Initialize an Array with 1000 in size that stores Double called 'transactions' to keep track of the user's transactions
+    double[] transactions = new double[1000];
+    int freeIndex = 0;
 
     public BankAccount(String name, int startingBalance){
-
+        this.name = name;
+        this.currentBalance = startingBalance;
     }
 
     public void deposit(double amount){
-
+        if(amount<=0){
+            System.out.println("Invalid Deposit");
+        }
+        else{
+            currentBalance += amount;
+            transactions[freeIndex++] =  amount;
+            System.out.println("Deposited " + amount + " from " + name +". New balance is " + currentBalance);
+        }
     }
 
     public void withdraw(double amount){
-
+        if(amount<=0 || amount>currentBalance){
+            System.out.println("Invalid Withdraw");
+        }
+        else{
+            currentBalance -= amount;
+            transactions[freeIndex++] =  -amount;
+            System.out.println("Withdrawn " + amount + " from " + name +". New balance is " + currentBalance);
+        }
     }
 
     public void displayTransactions(){
-
+        for(int i=0; i<freeIndex; i++){
+            System.out.print(transactions[i] + ", ");
+        }
     }
 
     public void displayBalance(){
-
+        System.out.println("Current Balance: " + currentBalance);
     }
 
     public static void main(String[] args) {
